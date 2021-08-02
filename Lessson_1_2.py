14e30ded297bff413f16cc08e305f6c27af74fbe
ChenChenIan <45452717+ChenChenIan@users.noreply.github.com>
2021-08-02 14:00:39 +0800

    Add files via upload

---
+++ Lessson_1_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 10:30:29 2021
+
+@author: iansu
+"""
+
+#挑戰一下! -- 讓電腦打招呼
+name = input('請問你叫甚麼名字? ') #用input讓使用者輸入名稱
+
+print(name, '你好!') #用print列印出名字及打招呼
