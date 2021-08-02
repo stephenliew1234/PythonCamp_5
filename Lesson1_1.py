754e3495eb1f58ea695da3cdb180cac078b07cde
ChenChenIan <45452717+ChenChenIan@users.noreply.github.com>
2021-08-02 14:00:29 +0800

    Add files via upload

---
+++ Lesson1_1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 16:02:54 2021
+
+@author: iansu
+"""
+#整數 ex. 10 or 5
+int()
+
+#浮點數 ex. 3.14 or 10.5
+float()
+
+#字串 ex. 'Hello World'
+str()
+
+#判斷類型
+type()
+
+#輸出文字到螢幕
+print()
+
+#得到使用者輸入資料
+input()
+
+#讓電腦打出 I'm Eric
+#          He is "cool"
+
+print("I'm Eric")
+print('He is "cool"')
