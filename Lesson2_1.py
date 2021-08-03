d3f809538a90690796c8de700ec0bf372f01c103
ChenChenIan <45452717+ChenChenIan@users.noreply.github.com>
2021-08-03 14:12:01 +0800

    Add files via upload

---
+++ Lesson2_1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 11:10:12 2021
+
+@author: iansu
+"""
+#課堂小測驗--Bug and Debug
+a = 3
+b = 1
+print(a+b)
+
+a = 10
+a = a+1
+print(a)
+
+x = input("輸入數字?")
+x = int(x)
+print(x/2)
